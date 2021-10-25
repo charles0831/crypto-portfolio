89d2c1b572c6a3653107a67e91ed69a99a403818
webdev0831 <fullstack831@gmail.com>
2021-10-25 20:51:42 +0200

    added: crypto portfolio

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,96 @@
+import React from "react";
+import "./../../index.css";
+
+const Contact = () => {
+  return (
+    <section className="contact-container" name="contact" id="contact">
+      <div className="profile-image"></div>
+      <h1 className="heading">Contact</h1>
+
+      <div className="cards-container">
+        {/* Card 1 - Email */}
+        <div className="cards">
+          <div className="contact-icons">
+            <i className="fas fa-envelope"></i>
+          </div>
+          <div className="contact-text">
+            <span>Email</span>
+          </div>
+          <div className="contact-links">
+            <span>fullstack831@gmail.com</span>
+          </div>
+        </div>
+
+        {/* Card 2 - Social Media */}
+        <div className="cards">
+          <div className="contact-icons">
+            <i className="fas fa-globe"></i>
+          </div>
+          <div className="contact-text">
+            <span>Social Media</span>
+          </div>
+          <div className="contact-links">
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://github.com/webdev0831/"
+            >
+              <i className="fab fa-github" title="github" id="github"></i>
+            </a>
+
+            {/* <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.facebook.com/profile.php?id=100041078220896123"
+            >
+              <i className="fab fa-facebook" title="facebook" id="facebook"></i>
+            </a> */}
+
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.linkedin.com/in/alexey-912296215/"
+            >
+              <i className="fab fa-linkedin" title="linkedin" id="linkedin"></i>
+            </a>
+
+            {/* <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://twitter.com/Oleg_Khurtin084869/"
+            >
+              <i className="fab fa-twitter" title="twitter" id="twitter"></i>
+            </a>
+
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.instagram.com/oOleg_Khurtinleg/"
+            >
+              <i
+                className="fab fa-instagram"
+                title="instagram"
+                id="instagram"
+              ></i>
+            </a> */}
+          </div>
+        </div>
+
+        {/* Card 3 - Phone */}
+        <div className="cards">
+          <div className="contact-icons">
+            <i className="fas fa-phone"></i>
+          </div>
+          <div className="contact-text">
+            <span>Phone</span>
+          </div>
+          <div className="contact-links">
+            <span>+</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,303 @@
+import React from "react";
+import "./../../index.css";
+import { motion } from "framer-motion";
+
+const boxVariants = {
+  hover: {
+    scale: 1.02,
+    transition: {
+      type: "spring",
+    },
+  },
+};
+
+const Portfolio = () => {
+  return (
+    <section
+      data-aos="fade-up"
+      className="portfolio-container"
+      name="portfolio"
+      id="portfolio"
+    >
+      <h1 className="heading">Projects I have Built</h1>
+      <div className="projects-container">
+        {/* Project - Diaries Manager App */}
+        <motion.div
+          variants={boxVariants}
+          whileHover="hover"
+          className="projects"
+        >
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://brainsshare.com/"
+          >
+            <div className="project-images" id="brainsshare"></div>
+          </a>
+          <div className="project-links">
+            <div className="text">
+              <h3>Brainsshare App</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/webdev0831/react-brainsshare"
+              >
+                <i className="fab fa-github" title="github repo" id="github">
+                  {" "}
+                </i>
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://brainsshare.com/"
+                target="_blank"
+              >
+                {" "}
+                <i
+                  className="fas fa-external-link-alt"
+                  title="live preview"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </motion.div>
+        {/* // */}
+
+        {/* Project - NFT Manager App */}
+        <motion.div
+          variants={boxVariants}
+          whileHover="hover"
+          className="projects"
+        >
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://nfa-v1.web.app/"
+          >
+            <div className="project-images" id="nftapp"></div>
+          </a>
+          <div className="project-links">
+            <div className="text">
+              <h3>NFT App</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/webdev0831/NFT-staking-app"
+              >
+                <i className="fab fa-github" title="github repo" id="github">
+                  {" "}
+                </i>
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://nfa-v1.web.app/"
+                target="_blank"
+              >
+                {" "}
+                <i
+                  className="fas fa-external-link-alt"
+                  title="live preview"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </motion.div>
+        {/* // */}
+
+        {/* Project - uniclix Manager App */}
+        <motion.div
+          variants={boxVariants}
+          whileHover="hover"
+          className="projects"
+        >
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://uniclixapp.com/"
+          >
+            <div className="project-images" id="uniclixapp"></div>
+          </a>
+          <div className="project-links">
+            <div className="text">
+              <h3>Uniclix App</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/webdev0831/uniclix_app"
+              >
+                <i className="fab fa-github" title="github repo" id="github">
+                  {" "}
+                </i>
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://uniclixapp.com/"
+                target="_blank"
+              >
+                {" "}
+                <i
+                  className="fas fa-external-link-alt"
+                  title="live preview"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </motion.div>
+        {/* // */}
+
+        {/* Project - Shoe Store */}
+        <motion.div
+          variants={boxVariants}
+          whileHover="hover"
+          className="projects"
+        >
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://pfron.nfinity.pl/"
+          >
+            <div className="project-images" id="pfronapp"></div>
+          </a>
+
+          <div className="project-links">
+            <div className="text">
+              <h3>PFRON app</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/webdev0831/pfron-frontend"
+              >
+                <i className="fab fa-github" title="github repo" id="github">
+                  {" "}
+                </i>
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://pfron.nfinity.pl/"
+                target="_blank"
+              >
+                {" "}
+                <i
+                  className="fas fa-external-link-alt"
+                  title="live preview"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </motion.div>
+        {/* // */}
+        {/* Project - Todo List */}
+        <motion.div
+          className="projects"
+          variants={boxVariants}
+          whileHover="hover"
+        >
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://ibs.nfinity.pl/"
+          >
+            <div className="project-images" id="ibsapp"></div>
+          </a>
+
+          <div className="project-links">
+            <div className="text">
+              <h3>IBS app</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/webdev0831/ibs-frontend"
+              >
+                <i
+                  className="fab fa-github"
+                  title="github repo"
+                  id="github"
+                ></i>
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://ibs.nfinity.pl/"
+                target="_blank"
+              >
+                <i
+                  className="fas fa-external-link-alt"
+                  title="live preview"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </motion.div>
+        {/* // */}
+        {/* Project - Blog Site */}
+        <motion.div
+          className="projects"
+          variants={boxVariants}
+          whileHover="hover"
+        >
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://gps.nfinity.pl/"
+          >
+            <div className="project-images" id="gpsapp"></div>
+          </a>
+
+          <div className="project-links">
+            <div className="text">
+              <h3>GPS app</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/webdev0831/react-material-ui-camera"
+              >
+                <i
+                  className="fab fa-github"
+                  title="github repo"
+                  id="github"
+                ></i>
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://gps.nfinity.pl/"
+                target="_blank"
+              >
+                <i
+                  className="fas fa-external-link-alt"
+                  title="live preview"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </motion.div>
+        {/* // */}
+      </div>
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/webdev0831"
+        className="button-link"
+      >
+        <button className="button">More Projects</button>
+      </a>
+    </section>
+  );
+};
+
+export default Portfolio;
